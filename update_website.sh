67e664b17bca6f8b845634789f164c4af24de22a
Manjyyot <manjyotsinghchaudhary@gmail.com>
2024-10-09 23:36:33 +0530

    updated shell script file

--- update_website.sh
+++ update_website.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 REPO_URL="https://github.com/Manjyyot/Building-CI-CD-Pipeline-Tool.git"
-REPO_DIR="/mnt/c/Users/tarun/devops/python/CI_CD/Building-CI-CD-Pipeline-Tool"  
+REPO_DIR="/mnt/c/Users/tarun/devops/python/CI_CD/Building-CI-CD-Pipeline-Tool"
 WEBSITE_DIR="/var/www/html"
 
 # Pull the latest changes from GitHub
@@ -9,9 +9,9 @@ cd $REPO_DIR || exit
 git pull origin main  # Make sure you are pulling from the correct branch
 
 # Copy the updated website to Nginx's web root
-sudo rsync -av --delete $REPO_DIR/ $WEBSITE_DIR/
+rsync -av --delete $REPO_DIR/ $WEBSITE_DIR/
 
 # Restart Nginx to apply changes
-sudo systemctl restart nginx
+systemctl restart nginx
 
 echo "Website updated successfully"
